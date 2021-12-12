52ae411b7c3950a60ca85a530582f7186d2b91ca
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2021-12-12 21:11:13 +0530

    deploy on heroku and date format fix with npm lib

--- LCO/socialapp/index.js
+++ LCO/socialapp/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 
-const PORT = 4000 || process.env.PORT;
+//date-format library
+var format = require('date-format');
+
+
+
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
   res.status(200).send("<h2>Hello there, I am Atharva!!!</h2>");
@@ -12,7 +17,7 @@ app.get('/api/v1/instagram', (req, res) => {
     "username" : "atharvaMahamuni",
     "followers" : "1,000,000",
     "follows" : "8",
-    "date" : Date.now()
+    "date" : format('Date: dd-MM-yyyy  Time: hh:mm:ss', new Date())
   };
 
   res.status(200).json(instaSocial);
@@ -24,7 +29,7 @@ app.get('/api/v1/facebook', (req, res) => {
     "username" : "atharvaMahamuni",
     "followers" : "1,000",
     "follows" : "1,000",
-    "date" : Date.now()
+    "date" : format('Date: dd-MM-yyyy  Time: hh:mm:ss', new Date())
   };
 
   res.status(200).json(faceSocial);
@@ -36,11 +41,17 @@ app.get('/api/v1/linkedin', (req, res) => {
     "username" : "atharvaMahamuni",
     "followers" : "500",
     "follows" : "80",
-    "date" : Date.now()
+    "date" : format('Date: dd-MM-yyyy  Time: hh:mm:ss', new Date())
   };
 
   res.status(200).json(linkSocial);
 
+});
+
+
+app.get('/api/v1/:token', (req, res) => {
+  console.log(req.params.token);
+  res.status(200).json({param: req.params.token});
 });
 
 app.listen(PORT, ()=>{
