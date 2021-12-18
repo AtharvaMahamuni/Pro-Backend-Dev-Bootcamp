781d969ec378e8dbf606a03259ffc3ac478a716d
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2021-12-18 21:35:31 +0530

    designed the user schema and dot env addition

--- LCO/lcoauthsystem/index.js
+++ LCO/lcoauthsystem/index.js
@@ -1,5 +1,8 @@
 const app = require('./app');
+const {PORT} = process.env;
 
-app.listen(4000, () => {
-    console.log('Server is listening on port 4000');
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
