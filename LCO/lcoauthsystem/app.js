2de93afed8e729a8e5d3b84e44c830a3b9902cad
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2021-12-21 20:33:51 +0530

    user reg, login and auth system refinement

--- LCO/lcoauthsystem/app.js
+++ LCO/lcoauthsystem/app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 require('./config/database').connect();
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('./model/user');
 
@@ -13,18 +15,79 @@ app.get('/', (req, res) => {
 });
 
 app.post('/register', async (req, res) => {
-    const {firstname, lastname, email, password} = req.body;
+    try{
+        const {firstname, lastname, email, password} = req.body;
 
-    if(!(firstname && lastname && email && password)){
-        res.status(400).send("All fields are required");
+        if(!(firstname && lastname && email && password)){
+            res.status(400).send("All fields are required");
+        }
+
+        const exisitingUser = await User.findOne({email}); //FIXME: PROMISE
+
+        if(exisitingUser){
+            res.status(401).send("User already exists");
+        }
+
+        const myEncPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            firstname,
+            lastname,
+            email: email.toLowerCase(),
+            password: myEncPassword
+        });
+
+        //Token generation
+        const token = jwt.sign(
+            {user_id: user._id, email}, 
+            process.env.SECRET_KEY,
+            {expiresIn: '2h'}
+        );
+        user.token = token;
+        //update or not in database
+
+        //handle password situation
+        //FIXME:  TO not expose encrypted password to the front
+        user.password = undefined
+
+        //send token or send just success yes and redirect - choice
+        res.status(201).json(user);
     }
-
-    const exisitingUser = await User.findOne({email}); //FIXME: PROMISE
-
-    if(exisitingUser){
-        res.status(401).send("User already exists");
+    catch (error){
+        console.log(error);
     }
+});
 
+app.post('/login', async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        if(!(email && password)){
+            res.status(400).send("All fields are required");
+        }
+
+        const user = await User.findOne({email});
+
+        // if(!user){
+        //     res.status(400).send("You are not registered in our app");
+        // }
+        // await bcrypt.compare(password, user.password);
+
+        if(user && (await bcrypt.compare(password, user.password))){
+            const token = jwt.sign(
+                {user_id: user._id, email},
+                process.env.SECRET_KEY,
+                {expiresIn: '2h'}
+            )
+            user.token = token;
+            user.password = undefined;
+            res.status(200).json(user);
+        }
+        res.send(400).send("email or password is incorrect");
+
+    } catch(error) {
+        console.log(error);
+    }
 });
 
 module.exports = app;
